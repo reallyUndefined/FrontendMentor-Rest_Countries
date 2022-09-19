815640715c67d76c2e1c8192dedd2c50eaa20b6f
Prabu <prabu.undefined@gmail.com>
2022-09-19 13:58:18 +0530

    fetching all countries with rtk query

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import { countriesApi } from "./api/countries";
 
 import themeReducer from "./theme";
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    [countriesApi.reducerPath]: countriesApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(countriesApi.middleware),
 });
 
 export const RtkProvider = ({ children }: React.PropsWithChildren) => (
